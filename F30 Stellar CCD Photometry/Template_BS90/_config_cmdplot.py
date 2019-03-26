13c14c4484baad0ef3e8c144114307d9f9b95417
Dora <anananlyjlyj@gmail.com>
2019-03-26 16:00:43 +0100

    Add files via upload

---
+++ F30 Stellar CCD Photometry/Template_BS90/_config_cmdplot.py
@@ -0,0 +1,10 @@
+#lst_match = "/path/to/list/<matched_stars>"  # define path to list of matched stars
+lst_match = "stars_combined.txt"
+shift = 18.6  # shift isochrones along y-axis, example value
+
+dct_iso = {}
+# the following entries are just example values
+# for more isochrones add more entries to dict_iso
+dct_iso["iso1"] = {"Age": 9.0e9, "Z": 0.0001, "color": "blue"}
+dct_iso["iso2"] = {"Age": 0.1e8, "Z": 0.004, "color": "red"}
+
---
+++ F30 Stellar CCD Photometry/Template_BS90/_config_matchstars.py
@@ -0,0 +1,7 @@
+# the lists created by STARFINDER (section 6.3)
+lst_psffit_V = "FoundStars_V_0.3.txt"  # define path to PSF fitting list for V filter
+lst_psffit_I = "FoundStars_I_0.4.txt"  # define path to PSF fitting list for I filter
+
+# the values of the zeropoints calculated in section 6.4
+zero_V = 25.15702
+zero_I = 25.14337 
